700003a923d3e7a2a1516f6e976bdef5e5b4ceb0
rvm <roberto.vallespin@gmail.com>
2018-03-18 21:08:32 +0100

    Primera revisión Reto 2.2

---
+++ retoP04/reto_2_2.asm
@@ -0,0 +1,60 @@
+run start
+org #8000
+
+.start
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Parte Fija
+ld a, #E2     
+ld (#c000), a 
+ld (#d000), a
+ld a, #F1     
+ld (#c800), a
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Parte Movil
+
+;; Frame 1
+ld hl, #F074     
+ld (#c001), hl 
+ld (#d001), hl
+ld hl, #F0F8     
+ld (#c801), hl
+ld hl, #F0F0
+ld (#c003), hl
+ld (#c803), hl
+ld (#d003), hl
+
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+
+;; Frame 1
+ld hl, #7400     
+ld (#c001), hl 
+ld (#d001), hl
+ld hl, #F800     
+ld (#c801), hl
+ld hl, #F0F0
+ld (#c003), hl
+ld (#c803), hl
+ld (#d003), hl
+
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+
+
+jp start
