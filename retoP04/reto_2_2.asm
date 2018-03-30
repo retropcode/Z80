5c71c42210c09ca92b0ba47d64ddb658f53f9007
rvm <roberto.vallespin@gmail.com>
2018-03-30 18:49:49 +0200

    Reto 2.2 Terminado

--- retoP04/reto_2_2.asm
+++ retoP04/reto_2_2.asm
@@ -35,7 +35,7 @@ halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
 halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
 halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
 
-;; Frame 1
+;; Frame 2
 ld hl, #7400     
 ld (#c001), hl 
 ld (#d001), hl
@@ -45,6 +45,27 @@ ld hl, #F0F0
 ld (#c003), hl
 ld (#c803), hl
 ld (#d003), hl
+
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
+
+;; Frame 3
+ld hl, #0000     
+ld (#c001), hl 
+ld (#d001), hl
+ld (#c801), hl
+ld hl, #F074
+ld (#c003), hl
+ld (#d003), hl
+ld hl, #F0F8
+ld (#c803), hl
 
 halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
 halt: halt: halt: halt: halt: halt: halt: halt: halt: halt
