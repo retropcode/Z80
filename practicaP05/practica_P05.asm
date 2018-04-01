41a0ed83e1c02413f4435ef066cdd3c18fd15625
rvm <roberto.vallespin@gmail.com>
2018-04-01 12:55:31 +0200

    Practica P05 Terminada

--- practicaP05/practica_P05.asm
+++ practicaP05/practica_P05.asm
@@ -3,3 +3,34 @@ run start
 org #8000
 
 .start
+          
+ld b, #14       ;; FOR b=20 TO 1
+.repet 		;; Parpadea 20 veces
+
+ld hl, #c000	;; Apuntamos a la pantalla
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Empezamos pintando 4 pixeles en "Fondo"
+ld (hl), #00
+
+ld c, #1D       ;; FOR c=30 
+.wait_1         ;;  Esperamos 30 interrupciones del CRT
+halt            ;;  que equivale a  100 milisegundos
+dec c
+jr nz, wait_1   ;; NEXT c
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ahora 4 pixeles en "Rojo"
+ld (hl), #FF
+
+ld c, #1D       ;; FOR c=30 
+.wait_2         ;;  Esperamos 30 interrupciones del CRT
+halt            ;;  que equivale a  100 milisegundos
+dec c
+jr nz, wait_2   ;; NEXT c
+
+dec b
+jr nz, repet    ;; NEXT b
+
+.finish
+jr finish
