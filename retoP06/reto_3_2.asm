740bec214a2cde45648a1d503b02cdc897e0d2f7
rvm <roberto.vallespin@gmail.com>
2018-04-08 19:39:34 +0200

    Reto 3.2 Terminado

--- retoP06/reto_3_2.asm
+++ retoP06/reto_3_2.asm
@@ -3,7 +3,51 @@ run start
 org #8000
 
 .start
-          
+ 
+ld a,#28        ; Repetir 40 veces el código
+
+ld hl, #C0A0    ; linea 1
+
+.repetir
+ld (hl), #FF    ;   1º byte
+inc l
+ld (hl), #FF    ;   2º byte
+
+ld h, #C8       ; linea 2
+ld (hl), #F0    ;   2º byte
+dec l
+ld (hl), #F0    ;   1º byte
+
+ld h, #D0       ; linea 3
+ld (hl), #08    ;   1º byte
+inc l
+ld (hl), #00    ;   2º byte
+
+ld h, #D8       ; linea 4
+ld (hl), #01    ;   2º byte
+dec l
+ld (hl), #04    ;   1º byte
+
+ld h, #E0       ; linea 5
+ld (hl), #02    ;   1º byte
+inc l
+ld (hl), #02    ;   2º byte
+
+ld h, #E8       ; linea 6
+ld (hl), #04    ;   2º byte
+dec l
+ld (hl), #01    ;   1º byte
+
+ld h, #F0       ; linea 7
+ld (hl), #F0    ;   1º byte
+inc l
+ld (hl), #78    ;   2º byte
+
+ld h, #c0
+inc l
+dec a
+
+jr nz, repetir
 
 .finish
 jr finish
