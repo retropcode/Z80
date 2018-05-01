41150df58573fb7f7a3c5fc9970f2dd48a711fd2
rvm <roberto.vallespin@gmail.com>
2018-05-01 22:08:34 +0200

    Se anima el personaje

--- retoP06/reto_3_4.asm
+++ retoP06/reto_3_4.asm
@@ -2,10 +2,43 @@
 run start
 org #8000
 
+.sprPersonaje
+defb #60,#00; line 0
+defb #D0,#00; line 1
+defb #68,#00; line 2
+defb #66,#00; line 3
+defb #77,#00; line 4
+defb #33,#08; line 5
+defb #06,#00; line 6
+defb #05,#00; line 7
+
+
+.sprPersonajeGun
+defb #60,#00; line 0
+defb #D0,#00; line 1
+defb #68,#00; line 2
+defb #66,#0C; line 3
+defb #77,#00; line 4
+defb #22,#00; line 5
+defb #06,#00; line 6
+defb #05,#00; line 7
+
+
+.sprPersonajeFire
+defb #60,#00; line 0
+defb #D0,#00; line 1
+defb #68,#08; line 2
+defb #CD,#31; line 3
+defb #EE,#00; line 4
+defb #44,#00; line 5
+defb #06,#00; line 6
+defb #05,#00; line 7
+
 .start
  
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Vamos a pintar el Cielo
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ld a,#28        ; Repetir 40 veces el código
 ld hl, #C000    ; linea 1
 
@@ -57,8 +90,9 @@ inc l
 dec a
 jr nz, RepCielo1
 
-
-;; Vamos a pintar el Cielo
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vamos a pintar el Cielo linea 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ld a,#28        ; Repetir 40 veces el código
 ld hl, #C050    ; linea 2
 
@@ -109,7 +143,9 @@ inc l
 dec a
 jr nz, RepCielo2
 
-;; Vamos a pintar el Suelo
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vamos a pintar el Suelo 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ld a,#28        ; Repetir 40 veces el código
 ld hl, #C0A0    ; linea 3
 
@@ -154,5 +190,96 @@ inc l
 dec a
 jr nz, RepSuelo
 
+.sequence
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vamos a pintar el Personaje
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ld b,#28        ; LO GUARDAOS PARA REPETIR DISPARO
+
+ld de, sprPersonaje ; Apuntamos al primer byte del sprite
+
+ld hl, #C050    ; Apuntamos a la 1ª posicion de pantalla
+ld c, #8        ; Repetir 8 lineas
+
+.Personaje
+ld a, (de)      ; Carga byte del sprite en A
+ld (hl), a      ; lo escribe en pantalla   
+inc l           ; Apuntamos a los siguientes 4 pixeles
+inc de          ; Siguiente byte del sprite
+ld a, (de)      ; Carga 2º byte del sprite en A
+ld (hl), a      ; lo escribe en pantalla
+inc de		    ; Siguiente byte del sprite
+dec l           ; Volvemos a la primera fila de 4 pixeles
+ld a, #8        ; -
+add a, h        ;  | Apuntamos a la siguiente linea
+ld h, a         ; -
+dec c           
+jr nz, Personaje
+
+ld a, #20
+.pausa1
+halt
+dec a
+jr nz, pausa1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vamos a pintar el Personaje con pistola
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ld de, sprPersonajeGun ; Apuntamos al primer byte del siguiente sprite
+
+ld hl, #C050    ; Apuntamos a la 1ª posicion de pantalla
+ld c, #8        ; Repetir 8 lineas
+
+.PersonajeGun
+ld a, (de)      ; Carga byte del sprite en A
+ld (hl), a      ; lo escribe en pantalla   
+inc l           ; Apuntamos a los siguientes 4 pixeles
+inc de          ; Siguiente byte del sprite
+ld a, (de)      ; Carga 2º byte del sprite en A
+ld (hl), a      ; lo escribe en pantalla
+inc de		    ; Siguiente byte del sprite
+dec l           ; Volvemos a la primera fila de 4 pixeles
+ld a, #8        ; -
+add a, h        ;  | Apuntamos a la siguiente linea
+ld h, a         ; -
+dec c           
+jr nz, PersonajeGun
+
+ld a, #20
+.pausa2
+halt
+dec a
+jr nz, pausa2
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vamos a pintar el Personaje disparando
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ld de, sprPersonajeFire ; Apuntamos al primer byte del siguiente sprite
+
+ld hl, #C050    ; Apuntamos a la 1ª posicion de pantalla
+ld c, #8        ; Repetir 8 lineas
+
+.PersonajeFire
+ld a, (de)      ; Carga byte del sprite en A
+ld (hl), a      ; lo escribe en pantalla   
+inc l           ; Apuntamos a los siguientes 4 pixeles
+inc de          ; Siguiente byte del sprite
+ld a, (de)      ; Carga 2º byte del sprite en A
+ld (hl), a      ; lo escribe en pantalla
+inc de		    ; Siguiente byte del sprite
+dec l           ; Volvemos a la primera fila de 4 pixeles
+ld a, #8        ; -
+add a, h        ;  | Apuntamos a la siguiente linea
+ld h, a         ; -
+dec c           
+jr nz, PersonajeFire
+
+ld a, #20
+.pausa3
+halt
+dec a
+jr nz, pausa3
+
+
 .finish
-jr finish
+jr sequence
